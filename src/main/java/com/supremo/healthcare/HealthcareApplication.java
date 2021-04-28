cc73009d3fc4c1399df5ce408107a705fe7dc82e
Sankeerth Bhagodi <sankeerth@gmail.com>
2021-04-28 21:11:34 +0530

    Working First stable Spring Boot App Commit

--- src/main/java/com/supremo/healthcare/HealthcareApplication.java
+++ src/main/java/com/supremo/healthcare/HealthcareApplication.java
@@ -8,6 +8,7 @@ public class HealthcareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthcareApplication.class, args);
+		System.out.println("Testing");
 	}
 
 }
