83b7ecd9744923ac26d7de980313c75cbc79ff53
Sankeerth Bhagodi <sankeerth@gmail.com>
2021-05-01 21:14:33 +0530

    First complete Page with basic Structure

--- src/main/java/com/supremo/healthcare/controller/HomeController.java
+++ src/main/java/com/supremo/healthcare/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.supremo.healthcare.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        model.addAttribute("applicationTitle", "Bed Allocation Details");
         return "home";
     }
 }
